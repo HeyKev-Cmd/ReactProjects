ea0cb9db603183b00f8df284715deb97660ff79d
Esther Wei <swei2@masonlive.gmu.edu>
2023-10-05 00:56:52 -0400

    add esther assignment

---
+++ Esther/GiraffeTask1/Models/Intro.cs
@@ -0,0 +1,9 @@
+﻿namespace GiraffeTask1.Models
+{
+    public class Intro
+    {
+        public string Name {  get; set; }
+        public string Pet {  get; set; }
+        public int Birth {  get; set; }
+    }
+}
