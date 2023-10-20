0da1b5d233de4d96a7078dc0ff5bc488d239b721
HeyKev-Cmd <ke012685@gmail.com>
2023-10-19 21:21:34 -0400

    assignment for 10/19

--- Kevin/MyInfoApi/MyInfoApi/Controllers/MyInfoController.cs
+++ Kevin/MyInfoApi/MyInfoApi/Controllers/MyInfoController.cs
@@ -6,10 +6,7 @@ namespace MyInfoApi.Controllers
     [Route("[controller]")]
     public class MyInfoController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        
 
         private readonly ILogger<MyInfoController> _logger;
 
@@ -18,7 +15,7 @@ namespace MyInfoApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetMyInfo")]
         public IEnumerable<MyInfo> Get()
         {
             var myInfoList = new List<MyInfo>
